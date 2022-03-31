f38cc541f52d3c059f84a97481e26ddcdf44d544
Jairo97 <oliveirajairon10@gmail.com>
2022-03-31 20:34:36 -0300

    Criada a função comprimentar Python OO

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,8 @@
 class Pessoa:
-    pass
+    def comprimentar(self,nome):
+        return f'Olá {nome} seu id é: {id(self)}'
+
+if __name__ == '__main__':
+    pessoa = Pessoa()
+    print(pessoa.comprimentar('Dell'))
+
