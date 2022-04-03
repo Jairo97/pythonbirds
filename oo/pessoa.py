e42b2b617217e2263dabd917946fc117cc9b7172
Jairo97 <oliveirajairon10@gmail.com>
2022-04-02 21:30:41 -0300

    Criado atributo de classe olhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2 #Atributo default (Atributo de Classe)
     def __init__(self,*filhos,nome=None,idade=37):
         self.nome = nome
         self.idade = idade
@@ -8,7 +9,7 @@ class Pessoa:
         return f'Olá {self.nome} sua idade  é: {self.idade}'
 
 if __name__ == '__main__':
-    filho = Pessoa(nome= 'San')
+    filho = Pessoa(nome= 'San',idade=17)
     pessoa = Pessoa(filho,nome='Din')
     print(pessoa.comprimentar())
     for filho in pessoa.filhos:
@@ -18,3 +19,9 @@ if __name__ == '__main__':
     del pessoa.filhos
     print(pessoa.__dict__)
     print(filho.__dict__)
+    pessoa.olhos = 1
+    print(id(pessoa.olhos))
+    del pessoa.olhos
+    Pessoa.olhos = 3
+    print(Pessoa.olhos, pessoa.olhos, filho.olhos)
+    print(id(Pessoa.olhos),id(pessoa.olhos), id(filho.olhos))
